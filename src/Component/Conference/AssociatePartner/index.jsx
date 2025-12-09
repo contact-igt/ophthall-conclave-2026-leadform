016e02068580be8ad563b51b0f0f5cd2a2945347
iyyappanmanivelan <iyyappanmani03@gmail.com>
2025-12-09 13:03:17 +0530

    added sponser logo and next js latest version

--- src/Component/Conference/AssociatePartner/index.jsx
+++ src/Component/Conference/AssociatePartner/index.jsx
@@ -6,9 +6,9 @@ const AssociatePartner = ({ partnerlist, sponserlist }) => {
     <section className={styles.partner}>
       <div className="container">
         <div className="row">
-          <div className="col-xl-6">
+          <div className="col-12">
             <Title title={"Associate Partners"} />
-            <div className="d-flex justify-content-center flex-wrap align-items-center gap-5 mt-md-5 mt-4  ">
+            <div className="d-flex justify-content-evenly flex-wrap align-items-center gap-5 mt-md-5 mt-4  ">
               {partnerlist?.map((data, i) => (
                 <div className={`${styles.partnerimg} text-center`}>
                   <img src={data?.image} className="img-fluid" />
@@ -17,9 +17,9 @@ const AssociatePartner = ({ partnerlist, sponserlist }) => {
             </div>
           </div>
 
-          <div className="col-xl-6 mt-5 pt-2 pt-lg-5 pt-xl-0 mt-xl-0">
+          <div className="col-xl-12 mt-5 pt-2 pt-lg-5">
             <Title title={"Associate Sponsors"} />
-            <div className="d-flex justify-content-center flex-wrap align-items-center gap-5 mt-md-5 mt-4 ">
+            <div className="d-flex justify-content-evenly  flex-wrap align-items-center gap-5 mt-md-5 mt-4 ">
               {sponserlist?.map((data, i) => (
                 <div className={`${styles.partnerimg} text-center`}>
                   <img src={data?.image} className="img-fluid" />
